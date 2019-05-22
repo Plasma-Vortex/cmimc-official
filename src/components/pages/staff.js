5a5b2d050e5c763fa550891629da702d9e2ad866
Misha Ivkov <applepi216@gmail.com>
2019-05-22 11:03:07 -0700

    update staff pt. 8

--- src/components/pages/staff.js
+++ src/components/pages/staff.js
@@ -5,6 +5,8 @@ import _ from "lodash";
 
 import { Spinner } from "../utilities";
 
+const chunkSize = 4;
+
 const StaffCard = ({ staff: { name, photo, description, year } }) => (
   <Col s={12} m={6} l={3}>
     <div className="card">
@@ -19,7 +21,7 @@ const StaffCard = ({ staff: { name, photo, description, year } }) => (
   </Col>
 );
 
-const StaffP = ({ people: staff, chunkSize: chunkSize }) => (
+const StaffP = ({ staff }) => (
   <div>
     { 
       _.chunk(staff, chunkSize).map((staffRow, idx) => (
@@ -56,21 +58,22 @@ class Staff extends React.Component {
 
   render() {
     const { staff, pending } = this.state,
-          chunkSize = 4, currYear = 2020;
+        currYear = 2020;
     if (pending) return <Spinner />;
     if (!staff) return <div />;
     else {
-      console.log(staff);
-      console.log(staff.staff);
-      console.log(staff.staff.filter((value, index, array) => 
-            { return value.year.includes(currYear) === true;}));
+      // console.log(staff);
+      // console.log(staff.staff);
+      // console.log(staff.staff.filter((value, index, array) => 
+      //       { return value.year.includes(currYear) === true;}));
       return ( <Tabs>
         <Tab title="Current Staff" className="grey-text text-darken-4" active>
-          <StaffP staff={{ chunkSize: chunkSize, people: staff.staff }} />
+          <StaffP staff={ staff.staff.filter((value, index, array) => 
+            { return value.year.includes(currYear) === true; }) } />
         </Tab>
         <Tab title="Former Staff" className="grey-text text-darken-4">
-          <StaffP staff={{ chunkSize: chunkSize, people: staff.staff.filter((value, index, array) => 
-            { return value.year.includes(currYear) === false; }) }} />
+        <StaffP staff={ staff.staff.filter((value, index, array) => 
+            { return value.year.includes(currYear) === false; }) } />
         </Tab>
       </Tabs> )
     } 
