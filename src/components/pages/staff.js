5a1ed5b0e5590c6a0b9655557f51ce0bb0046b91
Misha Ivkov <applepi216@gmail.com>
2019-05-22 10:43:04 -0700

    update staff pt. 5

--- src/components/pages/staff.js
+++ src/components/pages/staff.js
@@ -1,6 +1,6 @@
 import React from "react";
 import fetch from "isomorphic-fetch";
-import { Row, Col } from "react-materialize";
+import { Row, Col, Tabs, Tab } from "react-materialize";
 import _ from "lodash";
 
 import { Spinner } from "../utilities";
@@ -17,6 +17,22 @@ const StaffCard = ({ staff: { name, photo, description, year } }) => (
       </div>
     </div>
   </Col>
+);
+
+const StaffP = ({ staff }) => (
+  <div>
+    { 
+      _.chunk(staff, chunkSize).map((staffRow, idx) => (
+        <Row className="staff-row" key={ idx }>
+          {
+            staffRow.map((staff, idx) => (
+              <StaffCard staff={ staff } key={ idx } />
+            ))
+          }
+        </Row>
+      ))
+    }
+  </div>
 );
 
 class Staff extends React.Component {
@@ -43,25 +59,18 @@ class Staff extends React.Component {
           chunkSize = 4, currYear = 2020;
     if (pending) return <Spinner />;
     if (!staff) return <div />;
-    else return (
-      <div>
-        { 
-          _.chunk(staff.staff.filter((value, index, array) => 
-            {
-              return value.year.includes(currYear);
-            }), 
-                  chunkSize).map((staffRow, idx) => (
-            <Row className="staff-row" key={ idx }>
-              {
-                staffRow.map((staff, idx) => (
-                  <StaffCard staff={ staff } key={ idx } />
-                ))
-              }
-            </Row>
-          ))
-        }
-      </div>
-    );
+    else {
+      return ( <Tabs>
+        <Tab title="Current Staff" className="grey-text text-darken-4" active>
+          <StaffP staff={ staff.staff.filter((value, index, array) => 
+            { return value.year.includes(currYear) === true; }) } />
+        </Tab>
+        <Tab title="Former Staff" className="grey-text text-darken-4">
+          <StaffP staff={ staff.staff.filter((value, index, array) => 
+            { return value.year.includes(currYear) === false; }) } />
+        </Tab>
+      </Tabs> )
+    } 
   }
 }
 
