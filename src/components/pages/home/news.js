1bf723c3947aebbb2eee89a02e06ef7428195d96
Plasma-Vortex <howardhalim@gmail.com>
2021-01-15 23:13:42 -0500

    Link in news: attempt 4

--- src/components/pages/home/news.js
+++ src/components/pages/home/news.js
@@ -40,7 +40,7 @@ class News extends React.Component {
                     <i className="fa fa-rss" aria-hidden="true" />
                   </Col>
                   <Col s={11}>
-                    <b>{ post.date }</b><p>{ post.message }</p>
+                    <b>{ post.date }</b><p>!{ html: post.message }</p>
                   </Col>
                 </Row>
               </li>
