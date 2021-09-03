e48cff35db4fb74fc936e0129603ca75fcabf988
Plasma-Vortex <howardhalim@gmail.com>
2021-09-03 02:44:03 -0400

    Update sponsor logos

--- src/components/pages/home/sponsors.js
+++ src/components/pages/home/sponsors.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const sponsors = [
   {
-    name: 'CMU SCS',
-    link: 'https://www.cs.cmu.edu/',
-    className: 'sponsor sponsor-cm'
+    name: 'SIG',
+    link: 'https://sig.com/',
+    className: 'sponsor sponsor-sig'
   },
   {
     name: 'Citadel',
@@ -12,9 +12,9 @@ const sponsors = [
     className: 'sponsor sponsor-ci'
   },
   {
-    name: 'Citadel Securities',
-    link: 'https://www.citadelsecurities.com/',
-    className: 'sponsor sponsor-cs'
+    name: 'Jane Street',
+    link: 'https://www.janestreet.com',
+    className: 'sponsor sponsor-js'
   },
   {
     name: 'DE Shaw',
@@ -27,19 +27,9 @@ const sponsors = [
     className: 'sponsor sponsor-2s'
   },
   {
-    name: 'Jane Street',
-    link: 'https://www.janestreet.com',
-    className: 'sponsor sponsor-js'
-  },
-  {
-    name: 'Expii',
-    link: 'https://www.expii.com',
-    className: 'sponsor sponsor-xp'
-  },
-  {
-    name: 'Wolfram',
-    link: 'https://www.wolfram.com',
-    className: 'sponsor sponsor-wa'
+    name: 'IMC',
+    link: 'https://www.imc.com/',
+    className: 'sponsor sponsor-imc'
   },
 ];
 
@@ -49,7 +39,7 @@ const Sponsors = () => (
       <h4>Sponsors</h4>
       {
         sponsors.map((sponsor, key) => (
-          <div className='col l3 m6 s12' key={key}>
+          <div className='col l4 m6 s12' key={key}>
             <a href={sponsor.link} target='_blank' className={sponsor.className}></a>
           </div>
         ))
